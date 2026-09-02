f29d33c7464ba1422fd34e8812f0aed28d781c7f
HishamAlkaed <HishamAlkaed@users.noreply.github.com>
2026-09-02 09:50:15 +0000

    deploy: HishamAlkaed/new_design@106b687474f7415dbd5fd268078aab0ffc6b57e8

--- assets/CnnDigitDemo-CiBPHZfs.js
+++ assets/CnnDigitDemo-CiBPHZfs.js
@@ -1,4 +1,4 @@
-import{c as K,r as g,j,m as G,A as _}from"./main-P8FKsWHd.js";/**
+import{c as K,r as g,j,m as G,A as _}from"./main--V8cQSpe.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
