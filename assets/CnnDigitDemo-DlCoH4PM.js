d1e1295b8bf742fdd7e1c86e410a0e9f1940e76f
HishamAlkaed <HishamAlkaed@users.noreply.github.com>
2026-09-02 09:36:37 +0000

    deploy: HishamAlkaed/new_design@663c60cbbb1e4aae473b4cdfac2188493ba9c4de

--- assets/CnnDigitDemo-DlCoH4PM.js
+++ assets/CnnDigitDemo-DlCoH4PM.js
@@ -1,4 +1,4 @@
-import{c as K,r as g,j,m as G,A as _}from"./main-CZC-hRx-.js";/**
+import{c as K,r as g,j,m as G,A as _}from"./main-P8FKsWHd.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
